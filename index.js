490912b0011eaf7b319edbe5d780e120c6437958
Mikko Pohja <mikko.pohja@futurice.com>
2017-08-03 12:20:34 +0300

    Command support and split for too long messages

--- index.js
+++ index.js
@@ -9,10 +9,41 @@ const bot = new TelegramBot(token, {polling: true});
 
 const client = new CIClient(CIconfig);
 
+const MAX_MESSAGE_LENGTH = 4096;
+const MAX_MESSAGES = 3;
+
+const splitMessage = (msg, maxLength) => {
+    // const messageCount = parseInt(msg.length / maxLength) + 1;
+    const messages = [];
+
+    while(msg.length > 0) {
+        let cut = msg.lastIndexOf(' ', maxLength);
+        if (cut > MAX_MESSAGE_LENGTH) {
+            cut = msg.lastIndexOf(',', maxLength); // Assuming JSON response
+        }
+        if (cut <= 0 && msg.length > 0) {
+            messages.push(msg);
+            break;
+        }
+        const messagePart = msg.slice(0, cut);
+        if (messagePart.length > 0) {
+            messages.push(messagePart);
+        }
+        msg = msg.substr(cut);
+    }
+    return messages;
+};
+
 const messageReceiver = (action, message, context) => {
-    console.log(`Message received from Central Intelligence ${message}`);
-    console.log(JSON.stringify(context, null, 2));
-    bot.sendMessage(context.msg.chat.id, message);
+    if (message.length > MAX_MESSAGE_LENGTH) {
+        console.log("Splitting too long message");
+        const messages = splitMessage(message, MAX_MESSAGE_LENGTH);
+        messages.forEach((msg, i) => {
+            setTimeout(() => bot.sendMessage(context.msg.chat.id, msg), i * 500);
+        });
+    } else {
+        bot.sendMessage(context.msg.chat.id, message);
+    }
 };
 
 client.setReceiver(messageReceiver);
@@ -20,6 +51,16 @@ client.setReceiver(messageReceiver);
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
+    if(msg.text && msg.text.indexOf('/') !== -1) {
+        const msgTokens = msg.text.split(' ');
+        const command = msgTokens.shift().split('/').pop();
+        const params = msgTokens.join(" ");
+        client.sendCommand(command, params, { msg });
+        return;
+    }
+
     // Forward message to Central Intelligence
-    client.sendMessage(msg.text, { msg });
+    if (msg.text && msg.text.split(" ").length > 2) {
+        client.sendMessage(msg.text, { msg });
+    }
 });
