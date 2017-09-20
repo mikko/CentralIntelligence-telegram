e1ab2b5e7e0319de6b0cc2dbae612600f154a778
Mikko Pohja <mikko.pohja@futurice.com>
2017-09-20 15:06:15 +0300

    DEfault trustedUserGroups

--- client-config.js
+++ client-config.js
@@ -6,5 +6,6 @@ module.exports = {
     serverPort: process.env.SERVER_PORT || 3000,
     myHost: process.env.MY_HOST || 'localhost',
     myPort: process.env.MY_PORT || 3004,
-    authKey: process.env.AUTH_KEY || fs.readFileSync('/run/secrets/authkey', { encoding: 'utf-8' }).trim()
+    authKey: process.env.AUTH_KEY || fs.readFileSync('/run/secrets/authkey', { encoding: 'utf-8' }).trim(),
+    trustedUserGroups: 'all',
 };
